b3e624a5f12899b265df0bc18862d2076591bb1c
muehlhaus <muehlhaus@bio.uni-kl.de>
2021-02-21 12:56:59 +0100

    change container to PlainJsonString

--- src/Cyjs.NET/Cytoscape.fs
+++ src/Cyjs.NET/Cytoscape.fs
@@ -7,13 +7,10 @@ open DynamicObj
 type Cytoscape() = 
     inherit DynamicObj ()
 
-    let tmpContainer = new System.Collections.Generic.List<DynamicObj>()
+    let tmpContainer  = "document.getElementById('cy')" |> PlainJsonString
     let tmpElements  = new System.Collections.Generic.List<DynamicObj>()
     let tmpStyle     = new System.Collections.Generic.List<DynamicObj>()
-    let tmpLayout    = new System.Collections.Generic.List<DynamicObj>()
-
-    member this.Add (item:#DynamicObj) = 
-        tmpContainer.Add(item) 
+    let tmpLayout    = new System.Collections.Generic.List<DynamicObj>() 
 
     member this.AddElement (element:#Element) = 
         tmpElements.Add(element) 
@@ -25,7 +22,7 @@ type Cytoscape() =
         tmpLayout.Add(item) 
 
 
-    member val ``container`` = tmpContainer with get,set 
+    member val ``container`` = tmpContainer  
     member val ``elements``  = tmpElements  with get,set 
     member val ``style``     = tmpStyle     with get,set 
     member val ``layout``    = tmpLayout    with get,set 

2629e69ad8d60fca5b1f1343f5709f90d38af1c6
Kevin Schneider <schneider.kev@outlook.de>
2023-03-22 19:27:02 +0100

    update to project-based buildchain, target netstandard2.0 only

---
+++ build/BasicTasks.fs
@@ -0,0 +1,41 @@
+﻿module BasicTasks
+
+open BlackFox.Fake
+open Fake.IO
+open Fake.DotNet
+open Fake.IO.Globbing.Operators
+
+open ProjectInfo
+
+let setPrereleaseTag = BuildTask.create "SetPrereleaseTag" [] {
+    printfn "Please enter pre-release package suffix"
+    let suffix = System.Console.ReadLine()
+    prereleaseSuffix <- suffix
+    prereleaseTag <- (sprintf "%s-%s" release.NugetVersion suffix)
+    isPrerelease <- true
+}
+
+let clean = BuildTask.create "Clean" [] {
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    ++ "tests/**/bin"
+    ++ "tests/**/obj"
+    ++ "pkg"
+    |> Shell.cleanDirs 
+}
+
+let build = BuildTask.create "Build" [clean] {
+    solutionFile 
+        |> DotNet.build (fun p ->
+            let msBuildParams =
+                {p.MSBuildParams with 
+                    Properties = ([
+                        "warnon", "3390"
+                    ])
+                }
+            {
+                p with 
+                    MSBuildParams = msBuildParams
+            }
+        )
+}

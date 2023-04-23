a239691e80e14bdeb29c03f5e6a50443be4f2fe1
Kevin Schneider <schneider.kev@outlook.de>
2023-04-23 11:56:28 +0200

    Use Giraffe.ViewEngine and DisplayOptions to generate HTML

---
+++ src/Cyjs.NET/Defaults.fs
@@ -0,0 +1,29 @@
+﻿namespace Cyjs.NET
+
+open Cyjs.NET.CytoscapeModel
+
+open DynamicObj
+open DynamicObj.Operators
+open System.Runtime.InteropServices
+
+open Giraffe.ViewEngine
+
+/// Contains mutable global default values.
+///
+/// Changing these values will apply the default values to all consecutive Graph generations.
+module Defaults =
+
+    /// The default width of the graph container in generated html files. Default: 600 (px)
+    let mutable DefaultWidth = 600
+
+    /// The default height of the graph container in generated html files. Default: 600 (px)
+    let mutable DefaultHeight = 600
+
+
+    let mutable DefaultDisplayOptions = DisplayOptions.initDefault()
+
+    /// reset global defaults to the initial values
+    let reset () =
+        DefaultWidth <- 600
+        DefaultHeight <- 600
+        DefaultDisplayOptions <- DisplayOptions.initDefault()
